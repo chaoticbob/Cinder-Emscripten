424ec96c2dc456de647591451810eabbe3b6e115
Hai Nguyen <chaoticbob@me.com>
2015-11-29 21:11:37 -0800

    Updated build configs for samples

--- src/cinder/gl/wrapper.cpp
+++ src/cinder/gl/wrapper.cpp
@@ -955,7 +955,7 @@ void readPixels( GLint x, GLint y, GLsizei width, GLsizei height, GLenum format,
 }
 
 // Compute
-#if defined( CINDER_MSW ) && ! defined( CINDER_GL_ANGLE )
+#if ( defined( CINDER_MSW ) && ! defined( CINDER_GL_ANGLE ) ) || ( defined( CINDER_LINUX ) && ! defined( CINDER_GL_ES ) )
 ivec3 getMaxComputeWorkGroupCount()
 {
 	ivec3 count;
@@ -973,7 +973,7 @@ ivec3 getMaxComputeWorkGroupSize()
 	glGetIntegeri_v( GL_MAX_COMPUTE_WORK_GROUP_SIZE, 2, &size.z );
 	return size;
 }
-#endif // defined( CINDER_MSW ) && ! defined( CINDER_GL_ANGLE )
+#endif // ( defined( CINDER_MSW ) && ! defined( CINDER_GL_ANGLE ) ) || ( defined( CINDER_LINUX ) && ! defined( CINDER_GL_ES ) )
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
